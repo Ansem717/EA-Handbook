bc41b1f5a2d890138c7b2b25cdfaa65c992c8717
Ansem717 <me@andymalik.com>
2022-05-29 10:21:38 -0700

    Implemented Navigation Bar, no functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,11 @@
-import logo from '../images/logo.svg';
-import '../css/App.css';
+import HeaderNavigation from './HeaderNavigation';
+import '../css/app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <HeaderNavigation />
+        
     </div>
   );
 }
