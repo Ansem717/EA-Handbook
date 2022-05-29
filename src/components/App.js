5b35652e9a27853a1c779ff56981964b66366333
Ansem717 <me@andymalik.com>
2022-05-29 11:08:54 -0700

    Implemented main content page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import HeaderNavigation from './HeaderNavigation';
+import Handbook from './Handbook';
 import '../css/app.css';
 
 function App() {
   return (
     <div className="app">
         <HeaderNavigation />
-        
+        <Handbook />
     </div>
   );
 }
